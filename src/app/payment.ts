5a2e6e6ae58886425eb4916497165b1f74a13bdb
PranavBodake <120270528+PranavBodake@users.noreply.github.com>
2023-04-09 16:27:26 +0530

    first commit

---
+++ src/app/payment.ts
@@ -0,0 +1,7 @@
+export class payment{
+  cardnumber!:number
+  name!:string
+  month!:string
+  year!:number
+  cvv!:number
+}
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,11 +9,30 @@ import { Router } from '@angular/router';
 })
 export class PaymentComponent implements OnInit {
 
+  payment = new FormGroup({
+    cardnumber : new FormControl('',{
+      validators :[Validators.required, Validators.maxLength(12)], nonNullable:true
+    }),
+    name: new FormControl('',{
+      validators:[Validators.required], nonNullable:true
+    }),
+    month: new FormControl('',{
+      validators:[Validators.required], nonNullable:true
+    }),
+    year: new FormControl('',{
+      validators:[Validators.required], nonNullable:true
+    }),
+    cvv: new FormControl('',{
+      validators:[Validators.required], nonNullable:true
+    }),
+  })
 
   ngOnInit(): void {
 
   }
   constructor( private router :Router){}
+
+
 
   paid():void{
     this.router.navigateByUrl('redirect')
